0e47c56473746c3fe7d8b1cf2076fe9fa63a7ed1
menglv <lihaili1987@163.com>
2016-10-05 10:41:20 +0800

    updat DockerFile

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,6 +4,6 @@ cd /webapps/app
 source /etc/profile.d/rvm.sh
 mkdir -p /webapps/shared/pids
 mkdir -p /webapps/shared/log
-cat /webapps/shared/pids/unicorn.pid
-kill -QUIT `cat /webapps/shared/pids/unicorn.pid`
+#cat /webapps/shared/pids/unicorn.pid
+#kill -QUIT `cat /webapps/shared/pids/unicorn.pid`
 bundle exec unicorn -E production -p 8080
